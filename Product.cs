2c2853fa1223cc9017bd86089eea7e122a1d146b
racadagrandy <githubrandy@gmail.com>
2024-11-22 23:44:38 +0800

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MODULE1GENERIC
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public override string ToString()
+        {
+            return $"ProductId: {ProductId}, ProductName: {ProductName}";
+        }
+    }
+}
